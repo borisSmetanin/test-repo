e6e0b8394c66667553d4a87a83fc612d9397722b
Boris Smetanin <boris@sortex.co.il>
2020-05-30 18:05:19 +0300

    Finished lesson VN lesson

---
+++ app/misc/vm.js
@@ -0,0 +1,33 @@
+/*
+* Example VM
+*
+* Running some arbitrary commands
+*/
+
+// Dependencies
+
+const vm = require('vm');
+
+// Define a context for the script to run in
+
+const context = {
+	foo: 25
+};
+
+// Define the script
+const script = new vm.Script(`
+
+	foo = foo * 2;
+	
+	var bar = foo +1;
+	
+	var fizz = 52;
+`);
+
+script.runInNewContext(context);
+
+console.log('context', context);
+
+
+
+
